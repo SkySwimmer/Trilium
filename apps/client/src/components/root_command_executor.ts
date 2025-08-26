aefb7e393ace56133225b8eaccd83a598a26f2dc
SkySwimmer <stefaneekhof@aerialworks.ddns.net>
2025-08-26 18:47:33 +0200

    Thats...bad, old code when i tried to have two pages for settings but never removed the incorrect statement

--- apps/client/src/components/root_command_executor.ts
+++ apps/client/src/components/root_command_executor.ts
@@ -118,8 +118,6 @@ export default class RootCommandExecutor extends Component {
     }
 
     async showOptionsCommand({ section }: CommandListenerData<"showOptions">) {
-        if (utils.isElectron())
-            return this.showOptionsCommand({ section });
         await appContext.tabManager.openContextWithNote(section || "_options", {
             activate: true,
             hoistedNoteId: "_options"
